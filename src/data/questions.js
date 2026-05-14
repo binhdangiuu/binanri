3fbb8de0575ba9f7e006b23cca73ddd8b77032da
Thanh Binh <tranthanhbinh07062006@gmail.com>
2026-05-14 13:37:20 +0700

    add new questions

--- src/data/questions.js
+++ src/data/questions.js
@@ -82,7 +82,155 @@ const questions = [
       "Tất cả đều sai"
     ],
     "answer": 2
-  }
+  },
+  {
+  "id": 8,
+  "chapter": "Performance",
+  "context": "Đoạn chương trình gồm 1000 lệnh trong đó lệnh load/store chiếm 30%, lệnh jump chiếm 10%, 20% lệnh rẽ nhánh, còn lại là các lệnh về đại số. Biết CPI của lệnh load/store là 2.5, lệnh jump là 1, lệnh rẽ nhánh là 1.5 và lệnh đại số là 2. Biết máy tính có tần số hoạt động là 2 GHz.",
+  "question": "Tính thời gian thực thi của đoạn chương trình trên?",
+  "options": [
+    "950 ns",
+    "975 ns",
+    "1000 ns",
+    "1950 ns"
+  ],
+  "answer": 1
+},
+{
+  "id": 9,
+  "chapter": "Performance",
+  "context": "Đoạn chương trình gồm 1000 lệnh trong đó lệnh load/store chiếm 30%, lệnh jump chiếm 10%, 20% lệnh rẽ nhánh, còn lại là các lệnh về đại số. Biết CPI của lệnh load/store là 2.5, lệnh jump là 1, lệnh rẽ nhánh là 1.5 và lệnh đại số là 2. Biết máy tính có tần số hoạt động là 2 GHz.",
+  "question": "Phải cải tiến CPI của lệnh load/store thành bao nhiêu để chương trình chạy nhanh gấp 1.5×?",
+  "options": [
+    "0.5",
+    "1",
+    "1.5",
+    "Tất cả đều sai"
+  ],
+  "answer": 3
+},
+{
+  "id": 10,
+  "chapter": "Performance",
+  "context": "Đoạn chương trình gồm 1000 lệnh trong đó lệnh load/store chiếm 30%, lệnh jump chiếm 10%, 20% lệnh rẽ nhánh, còn lại là các lệnh về đại số. Biết CPI của lệnh load/store là 2.5, lệnh jump là 1, lệnh rẽ nhánh là 1.5 và lệnh đại số là 2. Biết máy tính có tần số hoạt động là 2 GHz.",
+  "question": "Tính CPI trung bình của đoạn chương trình trên?",
+  "options": [
+    "1.25",
+    "1.5",
+    "1.95",
+    "2.0"
+  ],
+  "answer": 2
+},
+{
+  "id": 11,
+  "chapter": "Performance",
+  "context": "Đoạn chương trình gồm 1000 lệnh trong đó lệnh load/store chiếm 30%, lệnh jump chiếm 10%, 20% lệnh rẽ nhánh, còn lại là các lệnh về đại số. Biết CPI của lệnh load/store là 2.5, lệnh jump là 1, lệnh rẽ nhánh là 1.5 và lệnh đại số là 2. Biết máy tính có tần số hoạt động là 2 GHz.",
+  "question": "Người ta tiến hành cải tiến lệnh load/store sao cho CPI của nó giảm đi một nửa. Tính speedup của hệ thống?",
+  "options": [
+    "≈1.22",
+    "≈1.24",
+    "≈1.26",
+    "≈1.28"
+  ],
+  "answer": 1
+},
+{
+  "id": 12,
+  "chapter": "CPU",
+  "question": "Định luật Moore là định luật về điều gì?",
+  "options": [
+    "Sự gia tăng kích thước chip",
+    "Sự gia tăng giá thành sản xuất các chip",
+    "Sự gia tăng hiệu suất của các chip",
+    "Sự gia tăng số lượng các bóng bán dẫn trên chip"
+  ],
+  "answer": 3
+},
+{
+  "id": 13,
+  "chapter": "MIPS",
+  "question": "Lệnh nào sau đây là một lệnh MIPS hợp lệ?",
+  "options": [
+    "add $0, $t0, 5",
+    "sll $t0, $t1, 34",
+    "lw $t1, 0x08($t0)",
+    "andi $t0, $t1, $t2"
+  ],
+  "answer": 2
+},
+{
+  "id": 14,
+  "chapter": "MIPS",
+  "question": "Cho lệnh MIPS sau: add $s0, $s0, $s0 #add two registers. Chọn phát biểu đúng:",
+  "options": [
+    "Lệnh trên có lỗi biên dịch do toán hạng nguồn và đích giống nhau",
+    "Lệnh trên có lỗi thực thi do toán hạng nguồn và đích giống nhau",
+    "$s0 = $s0 * 2",
+    "Tất cả đều sai"
+  ],
+  "answer": 2
+},
+{
+  "id": 15,
+  "chapter": "MIPS",
+  "question": "Cho đoạn lệnh MIPS sau: lw $t0, 30($s3) # load word; add $s1, $s2, $t0. Giả sử $s3 chứa địa chỉ nền của một dãy integer, chọn phát biểu đúng:",
+  "options": [
+    "Đoạn lệnh trên có lỗi khi biên dịch",
+    "Đoạn lệnh trên có lỗi khi thực thi",
+    "$s1 = $s2 + 30*$s3",
+    "Tất cả đều sai"
+  ],
+  "answer": 3
+},
+{
+  "id": 16,
+  "chapter": "MIPS",
+  "question": "Chọn phát biểu đúng về các lệnh MIPS chuẩn sau:",
+  "options": [
+    "Lệnh lhu chuyển 16 bits dữ liệu từ bộ nhớ vào cả 16 bits cao lẫn 16 bits thấp",
+    "Lệnh lb chuyển 1 byte dữ liệu từ thanh ghi xuống bộ nhớ",
+    "Lệnh sb thực hiện việc mở rộng dấu dữ liệu",
+    "Lệnh lui chuyển giá trị tức thời vào 16 bits cao của thanh ghi"
+  ],
+  "answer": 3
+},
+{
+  "id": 17,
+  "chapter": "MIPS",
+  "question": "Để gán giá trị 0x12345678 cho thanh ghi $t0, cần thực hiện những lệnh MIPS chuẩn nào?",
+  "options": [
+    "lui $t0, 0x1234; lli $t0, 0x5678",
+    "lui $t0, 4660; ori $t0, $t0, 22136",
+    "addi $t0, $zero, 0x12345678",
+    "Tất cả đều đúng"
+  ],
+  "answer": 1
+},
+{
+  "id": 18,
+  "chapter": "MIPS",
+  "question": "Muốn đảo tất cả các bit trong thanh ghi $s1 thì cần thực hiện lệnh MIPS chuẩn nào sau đây?",
+  "options": [
+    "not $s0, $s1",
+    "nor $s0, $s1, $s1",
+    "andi $s0, $s1, -1",
+    "or $s0, $s1, $zero"
+  ],
+  "answer": 1
+},
+{
+  "id": 19,
+  "chapter": "MIPS",
+  "question": "Lệnh MIPS chuẩn nào sau đây gán giá trị 0 cho thanh ghi $s0?",
+  "options": [
+    "add $s0, $s0, $zero",
+    "ori $s0, $s0, 0",
+    "lui $s0, 0",
+    "Các câu A, B và C đều đúng"
+  ],
+  "answer": 2
+},
 ]
 
 export default questions
