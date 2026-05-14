482c3d8d1e65780a4e6c4c075a94b128241bd38b
Thanh Binh <tranthanhbinh07062006@gmail.com>
2026-05-14 13:25:26 +0700

    add new questions

--- src/data/questions.js
+++ src/data/questions.js
@@ -1,35 +1,87 @@
 const questions = [
   {
-    question: "What is polymorphism?",
-    options: [
-      "One method many forms",
-      "Memory allocation",
-      "Data hiding",
-      "Compilation"
+    "id": 1,
+    "chapter": "Introduction",
+    "question": "Đâu là sự phân loại các máy tính trong giai đoạn hiện tại?",
+    "options": [
+      "Máy tính nhúng, máy tính cá nhân, máy chủ và siêu máy tính",
+      "Thiết bị di động, máy tính cá nhân và máy chủ",
+      "Siêu máy tính, điện toán đám mây và máy tính cá nhân",
+      "Tất cả đều sai"
     ],
-    answer: 0
+    "answer": 0
   },
-
   {
-    question: "What does inheritance allow?",
-    options: [
-      "Code reuse",
-      "Deleting objects",
-      "Encrypting data",
-      "Compiling faster"
+    "id": 2,
+    "chapter": "Performance",
+    "question": "Yếu tố nào ảnh hưởng đến hiệu suất của một chương trình trên máy tính?",
+    "options": [
+      "Ngôn ngữ lập trình",
+      "Kiến trúc bộ xử lý",
+      "Kiến trúc tập lệnh",
+      "Tất cả đều đúng"
     ],
-    answer: 0
+    "answer": 3
   },
-
   {
-    question: "Which keyword is used for inheritance in Java?",
-    options: [
-      "implements",
-      "inherits",
-      "extends",
-      "super"
+    "id": 3,
+    "chapter": "Performance",
+    "question": "Hoạt động của hệ điều hành sẽ ảnh hưởng đến loại thời gian nào?",
+    "options": [
+      "Thời gian CPU - CPU time",
+      "Thời gian thực thi CPU - CPU execution time",
+      "Thời gian tổng thể - Elapsed time",
+      "Tất cả các loại thời gian kể trên"
     ],
-    answer: 2
+    "answer": 2
+  },
+  {
+    "id": 4,
+    "chapter": "Performance",
+    "question": "Để tăng hiệu suất của một ứng dụng, cách nào sau đây là khả thi?",
+    "options": [
+      "Giảm thời gian một chu kỳ của máy tính thực thi ứng dụng",
+      "Thay đổi ngôn ngữ lập trình để giảm số lượng lệnh máy",
+      "Thay đổi giải thuật để giảm số chu kỳ cần thiết cho chương trình",
+      "Tất cả các biện pháp trên"
+    ],
+    "answer": 3
+  },
+  {
+    "id": 5,
+    "chapter": "CPU",
+    "question": "Tại sao tần số xung clock của các bộ xử lý Intel bắt đầu giảm vào năm 2004?",
+    "options": [
+      "Do nhu cầu tính toán giảm",
+      "Gặp phải bức tường công suất",
+      "Chuyển sang các hệ thống đa nhân",
+      "Để giảm giá thành sản xuất do khủng hoảng kinh tế"
+    ],
+    "answer": 1
+  },
+  {
+    "id": 6,
+    "chapter": "Programming Languages",
+    "question": "Mức trừu tượng của chương trình giảm dần theo thứ tự nào?",
+    "options": [
+      "Ngôn ngữ cấp cao, ngôn ngữ máy, hợp ngữ",
+      "Ngôn ngữ máy, hợp ngữ, ngôn ngữ cấp cao",
+      "Ngôn ngữ cấp cao, hợp ngữ, ngôn ngữ máy",
+      "Hợp ngữ, ngôn ngữ máy, ngôn ngữ cấp cao"
+    ],
+    "answer": 2
+  },
+  {
+    "id": 7,
+    "chapter": "MIPS",
+    "question": "Đâu là phát biểu đúng trong ngữ cảnh bộ xử lý kiến trúc MIPS?",
+    "options": [
+      "Một lệnh máy tương đương nhiều lệnh ngôn ngữ lập trình C",
+      "Một lệnh cấp cao luôn phải cần nhiều lệnh máy",
+      "Một lệnh hợp ngữ tương đương một lệnh máy",
+      "Tất cả đều sai"
+    ],
+    "answer": 2
   }
 ]
 
