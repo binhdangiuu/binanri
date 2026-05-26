8c0dcf5261dca81186150ea1d796c77e6ac9fb23
Thanh Binh <tranthanhbinh07062006@gmail.com>
2026-05-26 18:28:53 +0700

    i

--- src/data/heap.js
+++ src/data/heap.js
@@ -30,22 +30,6 @@ const heap = [
     "answer": "A"
   },
   {
-    "id": 2,
-    "chapter": "Heap",
-    "question": {
-      "main": "Given an array consisting of 9 elements [a1, a2, ..., a9]. Suppose that after the process of building it into a heap, we obtain the following array:",
-      "sequence": [["a9", " a6", " a3", " a4", " a7", " a1", " a2", " a5", " a8"]],
-      "ask": "Which of the following sequences could be the result of sorting the original array in descending order?"
-    },
-    "options": {
-      "A": "[a9, a6, a7, a3, a4, a2, a1, a8, a5]",
-      "B": "[a9, a3, a1, a2, a4, a5, a6, a7, a8]",
-      "C": "[a9, a6, a7, a3, a1, a2, a5, a4, a8]",
-      "D": "[a6, a3, a9, a1, a4, a2, a7, a5, a8]"
-    },
-    "answer": "A"
-  },
-  {
     "id": 3,
     "chapter": "Heap",
     "question": {
@@ -61,6 +45,97 @@ const heap = [
     },
     "answer": "A"
   },
+  {
+    "id": 4,
+    "chapter": "Heap",
+    "question": {
+      "main": "Given that array A is used to represent a max-heap and an index i. Assuming that all element accesses in the array are valid, which of the following expressions is ALWAYS TRUE?",
+      "ask": "Choose the correct expression:"
+    },
+    "options": {
+      "A": "A[i] >= A[2i + 1]",
+      "B": "A[i] < A[2i + 2]",
+      "C": "A[i] > A[2i]",
+      "D": "A[i] < A[2i - 1]"
+    },
+    "answer": "A"
+  },
+{
+    "id": 5,
+    "chapter": "Heap",
+    "question": {
+      "main": "Given a min-heap represented as an array:",
+      "sequence": [1, 4, 7, 8, 10, 12, 13],
+      "ask": "Which of the following is a CORRECT representation for the state of the min-heap after deleting an element from it?"
+    },
+    "options": {
+      "A": "[4, 7, 8, 10, 13, 12]",
+      "B": "[4, 7, 8, 13, 10, 12]",
+      "C": "[4, 8, 13, 7, 10, 12]",
+      "D": "[4, 8, 7, 13, 10, 12]"
+    },
+    "answer": "D"
+  },
+  {
+    "id": 6,
+    "chapter": "Heap",
+    "question": {
+      "main": "What is the time complexity of the ReHeapUp operation?",
+      "ask": "Choose the correct time complexity:"
+    },
+    "options": {
+      "A": "O(log n)",
+      "B": "O(n)",
+      "C": "O(n log n)",
+      "D": "O(1)"
+    },
+    "answer": "A"
+  },
+  {
+    "id": 7,
+    "chapter": "Heap",
+    "question": {
+      "main": "What is the time complexity of the build heap operation?",
+      "ask": "Choose the correct time complexity:"
+    },
+    "options": {
+      "A": "O(log n)",
+      "B": "O(n)",
+      "C": "O(n log n)",
+      "D": "O(1)"
+    },
+    "answer": "B"
+  },
+  {
+    "id": 8,
+    "chapter": "Heap",
+    "question": {
+      "main": "What is the time complexity of heap sort?",
+      "ask": "Choose the correct time complexity:"
+    },
+    "options": {
+      "A": "O(log n)",
+      "B": "O(n)",
+      "C": "O(n log n)",
+      "D": "O(1)"
+    },
+    "answer": "C"
+  },
+  {
+    "id": 9,
+    "chapter": "Heap",
+    "question": {
+      "main": "What is the time complexity of the operation to delete 1 element from a heap?",
+      "ask": "Choose the correct time complexity:"
+    },
+    "options": {
+      "A": "O(log n)",
+      "B": "O(n)",
+      "C": "O(n log n)",
+      "D": "O(1)"
+    },
+    "answer": "A"
+  }
 
 ]
 export default heap
