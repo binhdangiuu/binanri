bb38bd67c2d46a47e9a293517c555e7d0d3de7e7
Thanh Binh <tranthanhbinh07062006@gmail.com>
2026-05-26 17:40:13 +0700

    i

--- src/data/heap.js
+++ src/data/heap.js
@@ -18,14 +18,14 @@ const heap = [
     "chapter": "Heap",
     "question": {
       "main": "Given an array consisting of 9 elements [a_1, a_2, \dots, a_9]. Suppose that after the process of building it into a heap, we obtain the following array:",
-      "sequence": ["a_9", "a_6", "a_3", "a_4", "a_7", "a_1", "a_2", "a_5", "a_8"],
+      "sequence": ["a9", "a6", "a3", "a4", "a7", "a1", "a2", "a5", "a8"],
       "ask": "What is the in-order traversal result?"
     },
     "options": {
-      "A": "[a_9, a_6, a_7, a_3, a_4, a_2, a_1, a_8, a_5]",
-      "B": "[a_9, a_3, a_1, a_2, a_4, a_5, a_6, a_7, a_8]",
-      "C": "[a_9, a_6, a_7, a_3, a_1, a_2, a_5, a_4, a_8]",
-      "D": "[a_6, a_3, a_9, a_1, a_4, a_2, a_7, a_5, a_8]"
+      "A": "[a9, a6, a7, a3, a4, a2, a1, a8, a5]",
+      "B": "[a9, a3, a1, a2, a4, a5, a6, a7, a8]",
+      "C": "[a9, a6, a7, a3, a1, a2, a5, a4, a8]",
+      "D": "[a6, a3, a9, a1, a4, a2, a7, a5, a8]"
     },
     "answer": "A"
   },
