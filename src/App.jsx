a1e0fc0b687a2f91cef0933b98100283cc1aa059
Thanh Binh <tranthanhbinh07062006@gmail.com>
2026-05-26 20:24:48 +0700

    i

--- src/App.jsx
+++ src/App.jsx
@@ -231,7 +231,7 @@ export default function App() {
       {/* LAST UPDATED */}
       <div className="text-center mb-4">
         <p className="text-gray-700 text-lg">
-          Last Updated: 26/05/2026 • 20:11:30 (DSA Heap added, DSA Hash is in progress))
+          Last Updated: 26/05/2026 • 20:11:30 (DSA Heap added, DSA Hash is in progress)
         </p>
       </div>
 
